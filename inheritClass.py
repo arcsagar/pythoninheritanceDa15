0830119176933a66023dc07039ae33dedcfe22f6
arcsagar <arcsagar15@gmail.com>
2025-04-05 18:01:10 +0530

    varients change

--- inheritClass.py
+++ inheritClass.py
@@ -1,12 +1,41 @@
 class royalEnfield:
     bikeName = 'baseName'
-    bikeVarients= [{
-        'model': 'base',
-        'price': 100
-    }]
     bikeColor = 'BaseColor'
+    bikeVarients= [
+        {
+        'model': 'classic350',
+        'price': 100,
+        'varient': 'blue'
+        },
+        {
+        'model': 'classic350',
+        'price': 110,
+        'varient': 'black'
+        },
+          {
+        'model': 'classic350',
+        'price': 130,
+        'varient': 'silver'
+        },
+          {
+        'model': 'hunter350',
+        'price': 90,
+        'varient': 'yellow'
+        },
+         {
+        'model': 'hunter350',
+        'price': 110,
+        'varient': 'green'
+        },
+         {
+        'model': 'hunter350',
+        'price': 150,
+        'varient': 'dualTone'
+        }
+    ]
 
-    def __init__(self,name = '',varients =[], color = ''):
+
+    def __init__(self,name = '',color = '', varients =[]):
         if name != '':
             self.bikeName = name
         if len(varients) != 0:
@@ -18,3 +47,5 @@ class royalEnfield:
         print(f'bike color is {self.bikeColor}')
         print(f'bike varients are  {self.bikeVarients}')
     
+
+classic350 = royalEnfield(name="classic 350", color='black', varients=[{}])
